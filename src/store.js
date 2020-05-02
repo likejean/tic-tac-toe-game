ecfa985cbbbe6567ebd2b4289c75324e42c53576
likejean <popachs@yahoo.com>
2020-05-01 22:00:07 -0700

    Cleanup store.js file

--- src/store.js
+++ src/store.js
@@ -5,24 +5,11 @@ import thunk from 'redux-thunk';
 const composeEnhancers =
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-        }) : compose;
-//Store Creation:
-//1. RootReducer
-//2. Redux DevTool Enhancer
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
 const enhancer = composeEnhancers(
-    applyMiddleware(thunk),
-    // other store enhancers if any
+    applyMiddleware(thunk)
 );
 export default createStore(rootReducer, enhancer);
 
 
-
-// export default createStore(
-//     rootReducer,
-//     compose(
-//         applyMiddleware(thunk),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-// );
